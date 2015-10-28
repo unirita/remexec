df95243416d2ba6bac91b0fec2f08e3883aa9210
Takahiro Honda <takahiro_honda@unirita.co.jp>
2015-10-28 17:25:58 +0900

    Fix mistake in function comment.

--- console/console.go
+++ console/console.go
@@ -22,7 +22,7 @@ func DisplayError(code string, a ...interface{}) (int, error) {
 	return fmt.Fprintln(os.Stderr, msg)
 }
 
-// GetMessage creates formatted message associated with code to stdout.
+// GetMessage creates formatted message associated with code.
 func GetMessage(code string, a ...interface{}) string {
 	return fmt.Sprintf("%s %s", code, fmt.Sprintf(msgs[code], a...))
 }
