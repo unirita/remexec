8a2d91e06e08e4006d9aa168f7d54e4cb4418b15
kazami <kazuhei_kazami@bsp.co.jp>
2015-10-29 13:33:47 +0900

    Add the process in the case of script.

--- executor/winrm.go
+++ executor/winrm.go
@@ -1,22 +1,25 @@
 package executor
 
 import (
+	"os/exec"
 	"strings"
 
 	"github.com/unirita/remexec/config"
 )
 
-var powershellTmpArg = `"& {invoke-command -ComputerName [remoteHost]
+var psExeTmpArgByCmd = `"& {invoke-command -ComputerName [remoteHost]
  -Credential (ConvertTo-SecureString [pass]
  -AsPlainText -Force | % { New-Object System.Management.Automation.PSCredential([userName], $_) } | % { Get-Credential $_ })
  -ScriptBlock{Invoke-Expression $args[0]}
  -argumentList [cmd]}"`
 
-var (
-	PowershellExeAbsPath  string
-	powershellExeOption   string
-	pwoershellExeArgument string
-)
+var psExeTmpArgByScript = `"& {invoke-command -ComputerName [remoteHost]
+ -Credential (ConvertTo-SecureString [pass]
+ -AsPlainText -Force | % { New-Object System.Management.Automation.PSCredential([userName], $_) } | % { Get-Credential $_ })
+ -File [script] }"`
+
+const powershellExeAbsPath = "powershell.exe"
+const powershellExeOption = "-Command"
 
 type WinrmExecutor struct {
 	host string
@@ -34,6 +37,12 @@ func NewWinrmExecutor(cfg *config.Config) *WinrmExecutor {
 
 func (e *WinrmExecutor) ExecuteCommand(command string) error {
 	// TODO: Call command with powershell.exe
+	cmdArg := createPSCommandArgument(e.host, e.user, e.pass, command)
+	cmd := exec.Command(powershellExeAbsPath, powershellExeOption, cmdArg)
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -43,9 +52,16 @@ func (e *WinrmExecutor) ExecuteScript(path string) error {
 	return nil
 }
 
-func createPowershellExeArgument(host, user, pass, cmd string) string {
+func createPSCommandArgument(host, user, pass, cmd string) string {
 	r := strings.NewReplacer("[remoteHost]", host, "[userName]", user, "[pass]", pass, "[cmd]", cmd)
-	arg := r.Replace(powershellTmpArg)
+	arg := r.Replace(psExeTmpArgByCmd)
+
+	return arg
+}
+
+func createPSScriptArgument(host, user, pass, script string) string {
+	r := strings.NewReplacer("[remoteHost]", host, "[userName]", user, "[pass]", pass, "[script]", script)
+	arg := r.Replace(psExeTmpArgByScript)
 
 	return arg
 }
