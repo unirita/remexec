b1190445bee992822dd988026c9ca418d1a8e5e6
kazami <kazuhei_kazami@bsp.co.jp>
2015-10-29 09:19:19 +0900

    Fix windows.go.

--- executor/executor_windows_test.go
+++ executor/executor_windows_test.go
@@ -1,35 +1,17 @@
 package executor
 
-import (
-	"reflect"
-	"testing"
-)
+import "testing"
 
-func TestNew_リモートホストのosがWindowsの場合はWindowsの構造体が作成(t *testing.T) {
-	exec := New("host", "user", "password", "windows")
-	if reflect.TypeOf(exec).String() != "*executor.Windows" {
-		t.Errorf("Windows構造体が設定されていない。 [%s]", reflect.TypeOf(exec).String())
+func TestNew_LocalWindowsAndRemoteWindows(t *testing.T) {
+	e := New("testhost", "testuser", "testpass", "windows")
+	if _, ok := e.(*PowershellExecuter); !ok {
+		t.Error("Type of executor must be PowershellExecuter, but was not.")
 	}
-
-	win, result := exec.(*Windows)
-	if !result {
-		t.Errorf("型アサーションに失敗")
-	}
-
-	if win.HostName != "host" {
-		t.Errorf("期待するホスト名が設定されていない。[%s]", win.HostName)
-	}
-
-	if win.UserName != "user" {
-		t.Errorf("期待するユーザ名が設定されていない。[%s]", win.UserName)
-	}
-
-	if win.Password != "password" {
-		t.Errorf("期待するパスワードが設定されていない。[%s]", win.Password)
-	}
-
 }
 
-func TestNew_リモートホストのosがLinuxの場合はLinuxの構造体が作成(t *testing.T) {
-
+func TestNew_LocalWindowsAndRemoteUnix(t *testing.T) {
+	e := New("testhost", "testuser", "testpass", "linux")
+	if _, ok := e.(*SSHExecuter); !ok {
+		t.Error("Type of executor must be SSHExecuter, but was not.")
+	}
 }
