794c39cf7de026a9dd1eb48278fb0f362055f00a
kazami <kazuhei_kazami@bsp.co.jp>
2015-11-06 11:04:00 +0900

    Add test.

--- executor/remexecps1_windows_test.go
+++ executor/remexecps1_windows_test.go
@@ -8,13 +8,38 @@ import (
 	"testing"
 )
 
+func makeLocalHostWinrmExecutor() *WinRMExecutor {
+	e := new(WinRMExecutor)
+	e.host = "localhost"
+	e.user = ""
+	e.pass = ""
+
+	return e
+}
+
 func TestRemexecPs1_InvalidArgument(t *testing.T) {
+	e := makeLocalHostWinrmExecutor()
+	remexecPs1 = filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "unirita", "remexec", "script", "remexec.ps1")
+	cmd := e.createCmd("ipconfig", WINRM_CMD)
+
+	cmd.Stderr = nil
+
+	rc, err := e.runAndGetRC(cmd)
+
+	if err != nil {
+		t.Fatalf("error occurred %s", err)
+	}
+
+	if rc != 0 {
+		t.Errorf("return code not match. rc => %d, expect => %d", rc, 0)
+	}
 
 }
 
 func TestRemexecPs1_NoExistCredential(t *testing.T) {
-	e := makeTestWinrmExecutor()
+	e := makeLocalHostWinrmExecutor()
 	e.host = "noexist"
+	e.pass = "misspass"
 	remexecPs1 = filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "unirita", "remexec", "script", "remexec.ps1")
 	cmd := e.createCmd("ipconfig", WINRM_CMD)
 
